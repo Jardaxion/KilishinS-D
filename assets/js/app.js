ae6545f0f11f883d2e0158429230629fd39d9b6e
Jardaxion <bainthecreator21@gmail.com>
2022-07-18 17:58:04 +0700

    'commit'

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,213 +1,5 @@
 AOS.init();
 
-// $(function() {
-
-// 	//Modal
-// 	$(".modal").each( function(){
-// 		$(this).wrap('<div class="modal__overlay"></div>')
-// 	});
-
-// 	$(".open-modal").on('click', function(e){
-// 		e.preventDefault();
-// 		e.stopImmediatePropagation;
-// 		var $this = $(this),
-// 		modal = $($this).data("modal");
-// 		$(".modal").not(modal).removeClass("open");
-// 		setTimeout( function(){
-// 			$(".modal").not(modal).parents(".modal__overlay").removeClass("open");
-// 			$("body").removeClass("nonescroll");
-// 		}, 350);
-// 		$(modal).parents(".modal__overlay").addClass("open");
-// 		setTimeout( function(){
-// 			$(modal).addClass("open");
-// 			$("body").addClass("nonescroll");
-// 		}, 350);
-
-// 		$(document).on('click', function(e){
-// 			var target = $(e.target);
-// 			if ($(target).hasClass("modal__overlay")){
-// 				$(target).find(".modal").each( function(){
-// 					$(this).removeClass("open");
-// 				});
-// 				setTimeout( function(){
-// 					$(target).removeClass("open");
-// 					$("body").removeClass("nonescroll");
-// 				}, 350);
-// 			}
-// 		});
-
-// 	});
-
-// 	$(".modal__close").on('click', function(e){
-// 		e.preventDefault();
-// 		e.stopImmediatePropagation;
-// 		var $this = $(this),
-// 		modal = $($this).data("modal");
-// 		$(modal).removeClass("open");
-// 		setTimeout( function(){	
-// 			$(modal).parents(".modal__overlay").removeClass("open");
-// 			$("body").removeClass("nonescroll");
-// 		}, 350);
-
-// 	});
-
-// 	//Mask phone
-// 	var telInp = $('input[type="tel"]');
-// 	telInp.each(function(){
-// 		$(this).mask("+7 (999) 999-99-99");
-// 	});
-
-// 	//Lang switcher
-// 	$(".lang a").on("click",function(e){
-// 		e.preventDefault();
-		
-// 		let lang = $(this).data("lang");
-
-// 		if (lang == 'ru') {
-// 			$(this).data("lang", 'eng');
-// 			$(this).text('eng');
-// 			$("html").attr("lang",'eng');
-// 		} else {
-// 			$(this).data("lang", 'ru');
-// 			$(this).text('ru');
-// 			$("html").attr("lang",'ru');
-// 		}
-// 	});
-
-// 	//Vacancy Value
-// 	$(".vacancy__list-link").on("click",function(e){
-// 		e.preventDefault();
-		
-// 		let vacancyVal = $(this).data('vacancy');
-
-// 		$('.input-vacancy').val(vacancyVal);
-// 	});
-
-// 	//Order button show
-// 	$('.form__input').on('keyup',function(){
-// 		valName = $('.input-name').val();
-// 		valPhone = $('.input-phone').val();
-
-// 		if(valName.length >= 1 && valPhone.length >= 1){
-// 			$('.order__head-hidden').addClass('active');
-// 		}else {
-// 			$('.order__head-hidden').removeClass('active');
-// 		}
-// 	});
-
-// 	//Validate Password show
-// 	$('.input-password').on('keyup',function(){
-// 		if($(this).val().length >= 1){
-// 			$('.modal__btn').removeClass('disabled');
-// 		}else {
-// 			$('.modal__btn').addClass('disabled');
-// 		}
-// 	});
-
-// 	//Validate Password Error Test
-// 	$('.modal__btn').on('click',function(e){
-// 		e.preventDefault();
-// 		$('.modal__form-item').addClass('error');
-// 	});
-
-// 	//Tabs
-// 	$('.tab-js').click(function(event){
-// 		event.preventDefault();
-// 		if(!$(this).hasClass('active')){
-// 			$('.tab-js').removeClass('active');
-// 			$(this).toggleClass("active");
-// 			$('.tab-content-js').hide();
-// 			var href = $(this).attr('href');
-// 			$(href).fadeIn();
-// 		}
-// 	});
-
-// 	//Range Slder Value
-// 	$('.form__range-input').on('input', function() {
-// 		var val = $(this).val();
-// 		var min = $(this).attr('min');
-// 		var max = $(this).attr('max');
-// 		var portion = (val - min) / (max - min);
-// 		$('.form__range-number span').text(val);
-// 	});
-
-// 	//Footer accordeon mobile
-// 	if ($(window).width() < '1025'){
-// 		$(".footer__link-title").on("click", function(event) {
-// 			$(this).toggleClass('active');
-// 			$(this).next().slideToggle(250);
-// 		});
-// 	}
-
-// 	//Sending a request Test
-// 	$("#form-vacancy").submit(function(event) {
-// 		// $.ajax({
-// 		// 	type: "POST",
-// 		// 	url: "assets/php/post-modal.php",
-// 		// 	data: $(this).serialize()
-// 		// }).done(function() { 
-// 		// 	$(".modal__massage").addClass("active").css('display', 'flex').hide().fadeIn(); 
-// 		// 	setTimeout(function(){ 
-// 		// 		jQuery("#form__modal").trigger("reset"); 
-// 		// 		$(".modal__massage").removeClass('active').fadeOut(); 
-// 		// 	}, 3000); 
-// 		// });
-// 		// return false;
-// 		event.preventDefault();
-// 		$(".massage").addClass("active").css('display', 'flex').hide().fadeIn(); 
-// 		setTimeout(function(){ 
-// 			jQuery("#form-vacancy").trigger("reset"); 
-// 			$(".massage").removeClass('active').fadeOut(); 
-// 		}, 3000); 
-// 	});
-
-// 	//Gallary
-// 	$('[data-fancybox]').fancybox({
-// 		animationEffect : "fade"
-// 	});
-
-// 	//Filtering works
-// 	$('.works-js').isotope({
-// 		itemSelector: '.works__item-js'
-// 	});
-
-// 	$('.works__category a').click(function(){
-// 		$('.works__category a').removeClass('active');
-// 		$(this).addClass('active');
-// 		var selector = $(this).attr('data-filter');
-
-// 		$('.works-js').isotope({
-// 			filter: selector,
-// 			animationOptions: {
-// 				duration: 1000,
-// 				easing: 'easeOutQuart',
-// 				queue: false
-// 			}
-// 		});
-// 		return false;
-// 	});
-
-// 	$('.slider__development-inner').slick({
-// 		arrows: false,
-// 		dots: true
-// 	})
-// });
-
-
-// 	//Hover development
-
-
-
-
-// $('.not-hover__item').on({
-// 	mouseenter: function() {
-// 		$(this).closest('.sites__inner-item--hover').css({'opacity' : '0', 'visibility':'hidden'});
-// 	},
-// 	mouseleave: function() {
-// 		$(this).closest('.sites__inner-item--hover').removeAttr( 'style' );
-// 	}
-// },);
-
 $(function() {
 
 	//Modal
@@ -266,21 +58,35 @@ $(function() {
 	});
 
 	//Lang switcher
-	$(".lang a").on("click",function(e){
-		e.preventDefault();
+	if($(window).width() >= '768'){
+		//PC
+		$('.lang a').on('click', function(e){
+			e.preventDefault();
 
-		let lang = $(this).data("lang");
+			$('.lang a._active').removeClass('_active');
 
-		if (lang == 'ru') {
-			$(this).data("lang", 'eng');
-			$(this).text('eng');
-			$("html").attr("lang",'eng');
-		} else {
-			$(this).data("lang", 'ru');
-			$(this).text('ru');
-			$("html").attr("lang",'ru');
-		}
-	});
+			$(this).addClass('_active');
+
+			$('html').attr('lang', $(this).data('lang'));
+		})
+	} else {
+		//mobile
+		$(".lang a").on("click",function(e){
+			e.preventDefault();
+	
+			let lang = $(this).data("lang");
+	
+			if (lang == 'ru') {
+				$(this).data("lang", 'eng');
+				$(this).text('eng');
+				$("html").attr("lang",'eng');
+			} else {
+				$(this).data("lang", 'ru');
+				$(this).text('ru');
+				$("html").attr("lang",'ru');
+			}
+		});
+	}
 
 	//Vacancy Value
 	$(".vacancy__list-link").on("click",function(e){
